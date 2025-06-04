e67ce5a53322d9c794a15fdef6297d5563c0627e
Checcolino <75864171+Checcolino1312@users.noreply.github.com>
2025-06-04 14:17:27 +0200

    Add navbar and home page

---
+++ fullstack-docker/frontend/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.css';
+
+const Home: React.FC = () => (
+  <div className="home">
+    <h1>Benvenuto nella Biblioteca</h1>
+    <p>Usa la barra di navigazione per esplorare l'app.</p>
+  </div>
+);
+
+export default Home;
